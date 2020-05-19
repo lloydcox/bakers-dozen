f67e729d508e7dcbc5b16bdf1aa25edd1e825c31
iixluuk3xii <36408448+iixluuk3xii@users.noreply.github.com>
2020-05-19 09:27:50 +0100

    Set up the file structure for the project

---
+++ Index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Baker's Dozen</title>
+</head>
+<body>
+<?php 
+include 'PHP/Header.php';
+
+include 'PHP/Main.php';
+
+include 'PHP/Footer.php';
+
+include 'PHP/Scripts.php'
+?>
+</body>
+</html>
