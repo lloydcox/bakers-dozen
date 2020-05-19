5b6021d1e53e7af32407c63cbbce1f687564b4f6
iixluuk3xii <36408448+iixluuk3xii@users.noreply.github.com>
2020-05-19 14:58:14 +0100

    Added click plugin for carousel and added jquery and slick script tags

--- Index.php
+++ Index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/Normalise.css">
+    <link rel="stylesheet" type="text/css" href="JS/slick/slick.css"/>
     <link rel="stylesheet" href="CSS/Style.css">
     <title>Baker's Dozen</title>
 </head>
--- PHP/Scripts.php
+++ PHP/Scripts.php
@@ -1,1 +1,3 @@
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://kit.fontawesome.com/4f920b82bf.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="JS/slick/slick.min.js"></script>
