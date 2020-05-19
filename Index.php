27e6e8db8dd4dfb977e7bdb92646bd8b1f69a889
iixluuk3xii <36408448+iixluuk3xii@users.noreply.github.com>
2020-05-19 11:36:46 +0100

    Updated the npm scss script to work correctly, added the normalise.css file to default the page, added a base style for box-sizing and default font

--- Index.php
+++ Index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/Normalise.css">
+    <link rel="stylesheet" href="CSS/Style.css">
     <title>Baker's Dozen</title>
 </head>
 <body>
