311fcbe950c245cb29928814fc639ed77d0e079f
iixluuk3xii <36408448+iixluuk3xii@users.noreply.github.com>
2020-05-21 14:19:13 +0100

    added in hover events to scss

--- PHP/QuickIcons.php
+++ PHP/QuickIcons.php
@@ -1,13 +1,13 @@
 <div id="QuickIcons">
-<div class="icon">
+<div class="icon bread">
 <i class="fas fa-bread-slice"></i>
 <h3>BREAD BAKED DAILY</h3>
 </div>
-<div class="icon">
+<div class="icon pastries">
 <i class="fas fa-cookie-bite"></i>
 <h3>HANDMADE SWEETS AND PASTRIES</h3>
 </div>
-<div class="icon">
+<div class="icon cake">
 <i class="fas fa-birthday-cake"></i>
 <h3>SPECIALLY MADE FOR YOUR EVENT</h3>
 </div>
