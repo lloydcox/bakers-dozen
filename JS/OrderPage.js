34dcd1435a0dca931745207ed0b4a9a6c16eb82a
iixluuk3xii <36408448+iixluuk3xii@users.noreply.github.com>
2020-05-29 15:44:36 +0100

    Continued to develop the JS for the order page

--- JS/OrderPage.js
+++ JS/OrderPage.js
@@ -1,18 +1,75 @@
+const $cost = document.querySelectorAll('[class$="-cost"]');
 const $slider = document.querySelectorAll('[class$="-ammount"]');
 const $number = document.querySelectorAll('[class$="-number"]');
 const $product = document.querySelectorAll('.item');
 const $pairs = [];
+const $basket = [];
+const $inBasket = [];
 
-class sliderPair {
-    constructor(number, slider, product) {
+const $cart = document.querySelector('#list');
+const $total = document.querySelector('#total')
+
+function notInArray(needle, haystack) {
+    let $count = haystack.length;
+    for(let i = 0; i<$count; i++){
+        if(haystack[i]===needle){
+            return false;
+        }
+    }
+    return true;
+}
+
+function arrayCount(array) {
+    let $count = array.length;
+    for(let i = 0; i<$count; i++){
+        return i;
+    }
+}
+
+class cartItem {
+    constructor($name, $quantity, $ammount){
+        this.name = $name;
+        this.quantity = $quantity;
+        this.totalAmmount = $ammount;
+        this.outPut = '' ;
+    }
+}
+
+class product {
+    constructor(number, slider, product, value) {
+
         this.number = number;
         this.slider = slider;
+        this.value = value;
         this.product = product;
+        this.cart = $basket.push(new cartItem(this.product.innerHTML, this.slider.value, this.number.innerText * this.value.innerText));
+        this.index = $pairs.length;
         this.number.innerText = this.slider.value;
-        this.slider.addEventListener('change', () => {this.number.innerText = this.slider.value;})
+
+        this.slider.addEventListener('change', () => {
+            this.number.innerText = this.slider.value;
+            $basket[this.index].quantity = this.slider.value;
+            $basket[this.index].totalAmmount = this.number.innerText * this.value.innerText;
+            $basket[this.index].outPut = '<div><h3>'+ $basket[this.index].name +'</h3><p class="'+ this.product.innerHTML.replace(/\s/g,'') +'-Quantity">Quantity: '+ $basket[this.index].quantity +'</p><p>'+ $basket[this.index].totalAmmount.toFixed(2) +'</p></div>'
+            
+            let $currentQuantity = '';
+            $basket[this.index].currentQuantity = $currentQuantity;
+
+            if($basket[this.index].quantity > 0) {
+                $currentQuantity = this.slider.value;
+                $inBasket.push($basket[this.index].outPut);
+            }
+            if($basket[this.index].quantity < 1) {
+                $inBasket.pop($basket[this.index].outPut);
+            } 
+            // if($basket[this.index].currentQuantity === $currentQuantity) {
+            //     if
+            // }
+            $cart.innerHTML = $inBasket.toString();
+        })
     }
 }
 
 $slider.forEach(() => {
-    $pairs.push(new sliderPair($number[($pairs.length)], $slider[($pairs.length)], $product[($pairs.length)]))
+    $pairs.push(new product($number[($pairs.length)], $slider[($pairs.length)], $product[($pairs.length)], $cost[($pairs.length)]))
 });
--- PHP/Internal/Inner_Includes/Order/AddToCart.php
+++ PHP/Internal/Inner_Includes/Order/AddToCart.php
@@ -1,27 +1,28 @@
 <div id="Shop">
     <div class="shop-content">
-    <div class="width">
-        <span class="divider"></span>
+        <div class="width">
+            <span class="divider"></span>
         </div>
-    <div class="bread-shop">
-        <?php
-        foreach ($Products as $Product) {
-            if ($Product['Category'] === 'Bread') {
-                echo '<div class="shop-item">
+        <div class="bread-shop">
+            <?php
+            foreach ($Products as $Product) {
+                if ($Product['Category'] === 'Bread') {
+                    echo '<div class="shop-item">
             <img src="' . $Product['Image_url'] . '" alt="' . $Product['Name'] . '">
             <div class="info">
                 <h3 class="item">' . $Product['Name'] . '</h3>';
-                if (!is_null($Product['Flavor1'])) {
-                    echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
-                    if (!is_null($Product['Flavor2'])) {
-                        echo ', ' . $Product['Flavor2'] . '</p>';
+                    if (!is_null($Product['Flavor1'])) {
+                        echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
+                        if (!is_null($Product['Flavor2'])) {
+                            echo ', ' . $Product['Flavor2'] . '</p>';
+                        } else {
+                            echo '</p>';
+                        }
                     } else {
-                        echo '</p>';
+                        echo '<div class="flavor"></div>';
                     }
-                } else {
-                    echo '<div class="flavor"></div>';
-                }
-                echo '</div>
+                    echo '<p class="' . $Product['id-name'] . '-cost">' . $Product['Price'] . '</p>
+                </div>
             <div class="quantity">
             <div class="value">
             <p class="' . $Product['id-name'] . '-number"></p>
@@ -29,29 +30,30 @@
             <input type="range" class="' . $Product['id-name'] . '-ammount" name="ammount" min="0" max="30" value="0">
             </div>
         </div>';
+                }
             }
-        }
-        ?>
+            ?>
         </div>
         <div class="cake-shop">
-        <?php
-        foreach ($Products as $Product) {
-            if ($Product['Category'] === 'Cakes') {
-                echo '<div class="shop-item">
+            <?php
+            foreach ($Products as $Product) {
+                if ($Product['Category'] === 'Cakes') {
+                    echo '<div class="shop-item">
             <img src="' . $Product['Image_url'] . '" alt="' . $Product['Name'] . '">
             <div class="info">
                 <h3 class="item">' . $Product['Name'] . '</h3>';
-                if (!is_null($Product['Flavor1'])) {
-                    echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
-                    if (!is_null($Product['Flavor2'])) {
-                        echo ', ' . $Product['Flavor2'] . '</p>';
+                    if (!is_null($Product['Flavor1'])) {
+                        echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
+                        if (!is_null($Product['Flavor2'])) {
+                            echo ', ' . $Product['Flavor2'] . '</p>';
+                        } else {
+                            echo '</p>';
+                        }
                     } else {
-                        echo '</p>';
+                        echo '<div class="flavor"></div>';
                     }
-                } else {
-                    echo '<div class="flavor"></div>';
-                }
-                echo '</div>
+                    echo '<p class="' . $Product['id-name'] . '-cost">' . $Product['Price'] . '</p>
+                </div>
             <div class="quantity">
             <div class="value">
             <p class="' . $Product['id-name'] . '-number"></p>
@@ -59,32 +61,33 @@
             <input type="range" class="' . $Product['id-name'] . '-ammount" name="ammount" min="0" max="30" value="0">
             </div>
         </div>';
+                }
             }
-        }
-        ?>
+            ?>
         </div>
         <div class="width">
-        <span class="divider"></span>
+            <span class="divider"></span>
         </div>
         <div class="pie-shop">
-        <?php
-        foreach ($Products as $Product) {
-            if ($Product['Category'] === 'Pie') {
-                echo '<div class="shop-item">
+            <?php
+            foreach ($Products as $Product) {
+                if ($Product['Category'] === 'Pie') {
+                    echo '<div class="shop-item">
             <img src="' . $Product['Image_url'] . '" alt="' . $Product['Name'] . '">
             <div class="info">
                 <h3 class="item">' . $Product['Name'] . '</h3>';
-                if (!is_null($Product['Flavor1'])) {
-                    echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
-                    if (!is_null($Product['Flavor2'])) {
-                        echo ', ' . $Product['Flavor2'] . '</p>';
+                    if (!is_null($Product['Flavor1'])) {
+                        echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
+                        if (!is_null($Product['Flavor2'])) {
+                            echo ', ' . $Product['Flavor2'] . '</p>';
+                        } else {
+                            echo '</p>';
+                        }
                     } else {
-                        echo '</p>';
+                        echo '<div class="flavor"></div>';
                     }
-                } else {
-                    echo '<div class="flavor"></div>';
-                }
-                echo '</div>
+                    echo '<p class="' . $Product['id-name'] . '-cost">' . $Product['Price'] . '</p>
+                </div>
             <div class="quantity">
             <div class="value">
             <p class="' . $Product['id-name'] . '-number"></p>
@@ -92,29 +95,30 @@
             <input type="range" class="' . $Product['id-name'] . '-ammount" name="ammount" min="0" max="30" value="0">
             </div>
         </div>';
+                }
             }
-        }
-        ?>
+            ?>
         </div>
         <div class="pizza-shop">
-        <?php
-        foreach ($Products as $Product) {
-            if ($Product['Category'] === 'Pizza') {
-                echo '<div class="shop-item">
+            <?php
+            foreach ($Products as $Product) {
+                if ($Product['Category'] === 'Pizza') {
+                    echo '<div class="shop-item">
             <img src="' . $Product['Image_url'] . '" alt="' . $Product['Name'] . '">
             <div class="info">
                 <h3 class="item">' . $Product['Name'] . '</h3>';
-                if (!is_null($Product['Flavor1'])) {
-                    echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
-                    if (!is_null($Product['Flavor2'])) {
-                        echo ', ' . $Product['Flavor2'] . '</p>';
+                    if (!is_null($Product['Flavor1'])) {
+                        echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
+                        if (!is_null($Product['Flavor2'])) {
+                            echo ', ' . $Product['Flavor2'] . '</p>';
+                        } else {
+                            echo '</p>';
+                        }
                     } else {
-                        echo '</p>';
+                        echo '<div class="flavor"></div>';
                     }
-                } else {
-                    echo '<div class="flavor"></div>';
-                }
-                echo '</div>
+                    echo '<p class="' . $Product['id-name'] . '-cost">' . $Product['Price'] . '</p>
+                </div>
             <div class="quantity">
             <div class="value">
             <p class="' . $Product['id-name'] . '-number"></p>
@@ -122,32 +126,33 @@
             <input type="range" class="' . $Product['id-name'] . '-ammount" name="ammount" min="0" max="30" value="0">
             </div>
         </div>';
+                }
             }
-        }
-        ?>
+            ?>
         </div>
         <div class="width">
-        <span class="divider"></span>
+            <span class="divider"></span>
         </div>
         <div class="pretzel-shop">
-        <?php
-        foreach ($Products as $Product) {
-            if ($Product['Category'] === 'Pretzels') {
-                echo '<div class="shop-item">
+            <?php
+            foreach ($Products as $Product) {
+                if ($Product['Category'] === 'Pretzels') {
+                    echo '<div class="shop-item">
             <img src="' . $Product['Image_url'] . '" alt="' . $Product['Name'] . '">
             <div class="info">
                 <h3 class="item">' . $Product['Name'] . '</h3>';
-                if (!is_null($Product['Flavor1'])) {
-                    echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
-                    if (!is_null($Product['Flavor2'])) {
-                        echo ', ' . $Product['Flavor2'] . '</p>';
+                    if (!is_null($Product['Flavor1'])) {
+                        echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
+                        if (!is_null($Product['Flavor2'])) {
+                            echo ', ' . $Product['Flavor2'] . '</p>';
+                        } else {
+                            echo '</p>';
+                        }
                     } else {
-                        echo '</p>';
+                        echo '<div class="flavor"></div>';
                     }
-                } else {
-                    echo '<div class="flavor"></div>';
-                }
-                echo '</div>
+                    echo '<p class="' . $Product['id-name'] . '-cost">' . $Product['Price'] . '</p>
+                </div>
             <div class="quantity">
             <div class="value">
             <p class="' . $Product['id-name'] . '-number"></p>
@@ -155,29 +160,30 @@
             <input type="range" class="' . $Product['id-name'] . '-ammount" name="ammount" min="0" max="30" value="0">
             </div>
         </div>';
+                }
             }
-        }
-        ?>
+            ?>
         </div>
         <div class="pastries-shop">
-        <?php
-        foreach ($Products as $Product) {
-            if ($Product['Category'] === 'Pastries') {
-                echo '<div class="shop-item">
+            <?php
+            foreach ($Products as $Product) {
+                if ($Product['Category'] === 'Pastries') {
+                    echo '<div class="shop-item">
             <img src="' . $Product['Image_url'] . '" alt="' . $Product['Name'] . '">
             <div class="info">
                 <h3 class="item">' . $Product['Name'] . '</h3>';
-                if (!is_null($Product['Flavor1'])) {
-                    echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
-                    if (!is_null($Product['Flavor2'])) {
-                        echo ', ' . $Product['Flavor2'] . '</p>';
+                    if (!is_null($Product['Flavor1'])) {
+                        echo '<p class="flavor">Notable Flavour/s: ' . $Product['Flavor1'];
+                        if (!is_null($Product['Flavor2'])) {
+                            echo ', ' . $Product['Flavor2'] . '</p>';
+                        } else {
+                            echo '</p>';
+                        }
                     } else {
-                        echo '</p>';
+                        echo '<div class="flavor"></div>';
                     }
-                } else {
-                    echo '<div class="flavor"></div>';
-                }
-                echo '</div>
+                    echo '<p class="' . $Product['id-name'] . '-cost">' . $Product['Price'] . '</p>
+                </div>
             <div class="quantity">
             <div class="value">
             <p class="' . $Product['id-name'] . '-number"></p>
@@ -185,14 +191,14 @@
             <input type="range" class="' . $Product['id-name'] . '-ammount" name="ammount" min="0" max="30" value="0">
             </div>
         </div>';
+                }
             }
-        }
-        ?>
+            ?>
         </div>
         <div id="cart">
             <h3></h3>
             <div id="list">
-
+                <!-- cart content -->
             </div>
             <h3 id="total"></h3>
         </div>
