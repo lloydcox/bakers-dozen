657f4c2a58e2a7477033e7341d07efff71d6bf14
iixluuk3xii <36408448+iixluuk3xii@users.noreply.github.com>
2020-06-01 14:44:17 +0100

    Cart total is now displayed correctly through the javscript

--- JS/OrderPage.js
+++ JS/OrderPage.js
@@ -4,9 +4,12 @@ const $number = document.querySelectorAll('[class$="-number"]');
 const $product = document.querySelectorAll(".item");
 const $Products = [];
 const $inCart = [];
+const $finalTotal = [];
 
 const $cart = document.querySelector("#list");
 const $total = document.querySelector("#total");
+
+// Check if item is not in an array ---------------------------
 
 function notInArray(needle, haystack) {
   let $count = haystack.length;
@@ -18,6 +21,19 @@ function notInArray(needle, haystack) {
   return true;
 }
 
+function sumArray(array) {
+  let $count = array.length;
+  let sum = 0;
+  for (let i = 0; i < $count; i++) {
+    sum += parseFloat(array[i]);
+  }
+  return sum.toFixed(2);
+}
+
+// Product class --------------------------------------------------
+
+$total.innerText = "Cart Total:";
+
 class product {
   constructor(number, slider, product, value) {
     this.quantity = number;
@@ -25,7 +41,8 @@ class product {
     this.slider = slider;
     this.price = value;
     this.name = product;
-    this.total = "£" + this.price * this.quantity.innerText;
+    this.total = (this.price * this.quantity.innerText).toFixed(2);
+    this.currentTotal = "";
     this.quantity.innerText = this.slider.value;
     this.outPut =
       "<div><h3>" +
@@ -34,14 +51,23 @@ class product {
       this.name.replace(/\s/g, "") +
       '-Quantity">Quantity: ' +
       this.slider.value +
-      "</p><p>" +
+      '</p><p class="' +
+      this.name.replace(/\s/g, "") +
+      '-total">£' +
       this.total +
       "</p></div>";
 
     this.slider.addEventListener("change", () => {
+      // Update Objects ----------------------------------------------
+
+      let $BasketTotal = $finalTotal.reduce((a, b) => {
+        return a + b;
+      }, 0);
+
       this.quantity.innerText = this.slider.value;
-      this.total = "£" + (this.price * this.quantity.innerText).toFixed(2);
+      this.total = (this.price * this.quantity.innerText).toFixed(2);
       this.cartIndex = $inCart.indexOf(this.outPut);
+      this.totalIndex = $finalTotal.indexOf(this.total);
       this.outPut =
         "<div><h3>" +
         this.name +
@@ -49,12 +75,15 @@ class product {
         this.name.replace(/\s/g, "") +
         '-Quantity">Quantity: ' +
         this.slider.value +
-        "</p><p>" +
+        '</p><p class="' +
+        this.name.replace(/\s/g, "") +
+        '-total">£' +
         this.total +
         "</p></div>";
 
+      // CartItem ------------------------------------
+
       if (
-        this.slider.value > 0 &&
         notInArray(this.currentOutput, $inCart) &&
         this.currentOutput !== this.outPut
       ) {
@@ -70,10 +99,35 @@ class product {
         this.currentOutput = "";
       }
 
+      //   Total ----------------------------------------------
+
+      if (
+        this.slider.value > 0 &&
+        notInArray(this.currentTotal, $finalTotal) &&
+        this.currentTotal !== this.total
+      ) {
+        this.currentTotal = this.total;
+        $finalTotal.push(this.total);
+      } else {
+        this.currentTotal = this.total;
+        $finalTotal.splice(this.totalIndex, 1, this.total);
+      }
+
+      if (this.slider.value < 1) {
+        $finalTotal.splice(this.totalIndex, 1);
+        this.currentTotal = "";
+      }
+
+      //   Update output ----------------------------------------------
+
       $cart.innerHTML = $inCart.toString();
+
+      $total.innerText = "Cart Total: £" + sumArray($finalTotal);
     });
   }
 }
+
+// Create product Array ---------------------------
 
 let count = $product.length;
 for (i = 0; i < count; i++) {
